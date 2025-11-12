8a04997358845d6fa603573b7327cae6c37fd69c
Krzysztof S3t <s3t000@gmail.com>
2025-11-12 16:53:11 +0100

    added database, added methods to get and save data in the database

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/go-api/db"
 	"example.com/go-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
@@ -18,7 +20,11 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get events: " + err.Error()})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -34,7 +40,10 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1 // later: get from authentication
 
-	event.Save()
-
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save event: " + err.Error()})
+		return
+	}
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
 }
