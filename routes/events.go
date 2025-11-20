e95dec0bec8b559119524c68008ecf343f583bcf
Krzysztof S3t <s3t000@gmail.com>
2025-11-20 16:31:36 +0100

    added route and method for updating event

--- routes/events.go
+++ routes/events.go
@@ -51,3 +51,33 @@ func createEvent(context *gin.Context) {
 	}
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID"})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get event: " + err.Error()})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "error: " + err.Error()})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event: " + err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully", "event": updatedEvent})
+}
