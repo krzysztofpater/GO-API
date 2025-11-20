847aa18f9911588440a824e88fbbb34b875d89a3
Krzysztof S3t <s3t000@gmail.com>
2025-11-20 15:07:57 +0100

    added routes package, reorganisation of the main.go file

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
 	"example.com/go-api/db"
-	"example.com/go-api/models"
+	"example.com/go-api/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,54 +10,8 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server) // we can pass the "server" instance because gin returns a pointer to that server
 
 	server.Run(":8080") // Start the server on port 8080
 
-}
-
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get events: " + err.Error()})
-		return
-	}
-	context.JSON(http.StatusOK, events)
-}
-
-func getEvent(context *gin.Context) {
-	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID"})
-		return
-	}
-	event, err := models.GetEventByID(eventId)
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get event: " + err.Error()})
-		return
-	}
-
-	context.JSON(http.StatusOK, event)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "error: " + err.Error()})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1 // later: get from authentication
-
-	err = event.Save()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save event: " + err.Error()})
-		return
-	}
-	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
 }
---
+++ routes/events.go
@@ -0,0 +1,53 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"example.com/go-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get events: " + err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID"})
+		return
+	}
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get event: " + err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "error: " + err.Error()})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1 // later: get from authentication
+
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save event: " + err.Error()})
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
+}
